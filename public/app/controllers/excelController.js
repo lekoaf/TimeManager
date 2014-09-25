11c1f44aa0601ee45b7439cc82cd89e978e895e8
martin <martin.troback@outlook.com>
2014-09-25 15:34:55 +0200

    /excel working

---
+++ public/app/controllers/excelController.js
@@ -0,0 +1,12 @@
+(function (){
+	angular.module('timeManager')
+	.controller('excelController', ['$scope', '$http', '$log', '$window', 
+		function ($scope, $http, $log, $window){
+
+		$scope.getExcelReport = function (report){
+			$log.log(report);
+
+			$window.location.href = '/excel/'+report.type+'/'+report.period;
+		};
+	}]);
+})();
