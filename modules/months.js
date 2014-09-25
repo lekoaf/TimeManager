de1a93544482625c8feef9dd3060a920ac94f4f6
martin <martin.troback@outlook.com>
2014-09-25 10:31:39 +0200

    /month working properly

--- modules/months.js
+++ modules/months.js
@@ -18,15 +18,10 @@ exports.months = function(month, time){
 		console.log('last');
 		var data = [];
 
-		var beginningOfLastMonth = moment().subtract(1,'months').day(0).hour(0).minute(0).second(0);
+		var beginningOfLastMonth = moment().subtract(1,'months').date(1).hour(0).minute(0).second(0);
 		var endOfLastMonth = beginningOfLastMonth.clone().add(1,'months');
 
 		for (var i=0; i<time.length; i++){
-			var dbMonth = time[i].date.split("-");
-			var nowMonth = moment().format('YYYY-MM-DD').split("-");
-			if (dbMonth[0] == nowMonth[0] && dbMonth[1] == nowMonth[1]){
-				continue;
-			}
 			var someDate = moment(time[i].date);
 			if (someDate.isAfter(beginningOfLastMonth) && someDate.isBefore(endOfLastMonth)){
 				data.push(time[i]);
