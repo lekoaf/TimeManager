0819f511284d135bbdfb030956201805406663c6
martin <martin.troback@outlook.com>
2014-09-26 13:15:35 +0200

    /admin working

--- public/app/controllers/adminController.js
+++ public/app/controllers/adminController.js
@@ -5,15 +5,24 @@
 		function ($scope, $log, $http){
 
 		$scope.users = [];
+		$scope.sortBy = '-statistics.lastlogin';
 
 		$scope.doSort = function (propName){
 			$scope.sortBy = propName;
 	        $scope.reverse = !$scope.reverse;
 		};
 
-		$scope.addOrRemoveAdmin = function (uid, type){
-			$http.put('/adminpermission/'+uid+'/'+type).success(function (data){
+		$scope.toggleAdmin = function (user){
+			
+			var type = 'mk';
+			
+			if (user.isadmin){
+				type = 'rm';
+			}
+
+			$http.put('/adminpermission/'+user._id+'/'+type).success(function (data){
 				$log.log(data);
+				getUsers();
 			}).error(function (data, status, header, config){
 				$log.log(data);
 			});
